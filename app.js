a0c86e24ff6d686b0137954769c805035d3a61ec
MilanTuryna <miloslavturyna@seznam.cz>
2021-01-22 14:06:52 +0100

    (app.js) Created constant elements with submit & clear element. Created constant messages with deleteConfirm message. Manipulation with events (addEventListener) added to end of script. All variables instanced in addText() dded to beginning of function. Changed while loop condition in clearAllText() because clearAllList.firstChild can't be string.

--- app.js
+++ app.js
@@ -1,64 +1,41 @@
-//consts with html tag
-const submit = document.getElementsByClassName('submit');
-const clear = document.getElementsByClassName('clear');
+const elements = {
+    submit: document.getElementsByClassName('submit'),
+    clear: document.getElementsByClassName('clear'),
+};
 
-//all listeners for function 
-submit[0].addEventListener('click', addText);
-clear[0].addEventListener('click', clearAllText);
+const messages = {
+    deleteConfirm: 'chcete túto položku odstranit',
+};
 
-//the function adds text and a list of created elements > li> input
 function addText(){
-    //creating new element li for element list
-    let myList = document.querySelector('#list');
-    let newList = document.createElement('li');
-    const input = document.getElementById('Input').value;
-    newList.textContent = input;
+    let myList = document.querySelector('#list'),
+        newList = document.createElement('li'),
+        newInputs = document.createElement('input'),
+        ol = document.querySelectorAll("li input"),
+        inputValue = document.getElementById('Input').value;
+
+    newList.textContent = inputValue;
     myList.appendChild(newList);
 
-    //creating new element input type="chackbox" for element li
-    let newInputs = document.createElement('input');
     newInputs.type = "checkbox";
     newInputs.className = 'box';
     newList.appendChild(newInputs);
 
-    /*******************************************************
-    *let ul = document.querySelectorAll("ul li")          *   
-    *ul.forEach((row, index) => row.id = "item-" + index) *
-    *******************************************************/
-    //each input gets a unique ID
-    let ol = document.querySelectorAll("li input")
-    ol.forEach((newInputs, newList) => newInputs.id = "item-" + newList)    
-    
-    //if you select some item in the list and confirm, it will be remove
+    ol.forEach((newInputs, newList) => newInputs.id = "item-" + newList);
+
     addEventListener('click', inputRemove => {
-        if(inputRemove.target !== newList && inputRemove.target !== newInputs){
-            return
-        }
-
-        let question = confirm('chcete túto položku odstranit?')
-    
-        if(question === true){
-            myList.removeChild(newList)
-        } else if(question === false){
-            //do nothing
-        }
-
+        if(inputRemove.target !== newList && inputRemove.target !== newInputs) return;
+        if(confirm(messages.deleteConfirm)) myList.removeChild(newList)
     });
-    
-    
-    // after clicking on Send, the message is added to the LI field and deleted on the input
-    if(input !== ' '){
-        const input = document.getElementById('Input').value = ' ';
-    }
+
+    if(inputValue !== ' ') document.getElementById('Input').value = ' ';
 }
 
-
-//for function click in CL clearing all items in list
 function clearAllText(){
-    //after clicking on the CL loop, it will delete all items
     let clearAllList = document.getElementById('list');
-
-    while(clearAllList.firstChild !== ' '){
-        clearAllList.removeChild(clearAllList.firstChild)
-    }
+    while(clearAllList.firstChild !== null) clearAllList.removeChild(clearAllList.firstChild)
 }
+
+
+elements.submit[0].addEventListener('click', addText);
+elements.clear[0].addEventListener('click', clearAllText);
